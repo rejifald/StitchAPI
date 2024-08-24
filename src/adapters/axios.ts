b1fb72eb38b274221d44a3a11cefc16caf53e13d
Oleksandr Zhuravlov <rejifald@gmail.com>
2024-08-24 09:06:23 +0300

    feat: merge adapter options

--- src/adapters/axios.ts
+++ src/adapters/axios.ts
@@ -1,18 +1,20 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { AdapterInput } from "../types/adapter";
+import merge from "lodash/merge";
 
 export const axiosAdapter =
   (initial: AxiosRequestConfig = {}) =>
   async ({ url, method, body }: AdapterInput) => {
-    const response = await axios({
-      ...initial,
-      url,
-      method,
-      data: body,
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    });
+    const response = await axios(
+      merge(initial, {
+        url,
+        method,
+        data: body,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }),
+    );
     return response.data;
   };
--- src/adapters/fetch.ts
+++ src/adapters/fetch.ts
@@ -1,17 +1,20 @@
 import isEmpty from "lodash/isEmpty";
 import { AdapterInput } from "../types/adapter";
+import merge from "lodash/merge";
 
 export const fetchAdapter =
   (initial: RequestInit = {}) =>
   async ({ url, method, body }: AdapterInput): Promise<unknown> => {
-    const response = await fetch(url, {
-      ...initial,
-      method,
-      body: isEmpty(body) ? undefined : JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    });
+    const response = await fetch(
+      url,
+      merge(initial, {
+        method,
+        body: isEmpty(body) ? undefined : JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }),
+    );
     return response.json();
   };
