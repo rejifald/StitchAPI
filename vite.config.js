175f54070ae41dd7ec9db25eb2ac81a8f74c9524
Oleksandr Zhuravlov <rejifald@gmail.com>
2024-08-25 16:28:05 +0300

    chore: config aliases

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import path from "path";
 
 export default defineConfig({
   plugins: [dts()],
+  alias: {
+    // eslint-disable-next-line no-undef
+    "@/": path.resolve(__dirname, "src"),
+  },
   build: {
     lib: {
       entry: "src/index.ts",
